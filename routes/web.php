fe29b998a375a4b3e610967f9aae5f889546d41e
kanouao <67881390+neontetoraru@users.noreply.github.com>
2022-05-02 21:57:30 +0900

    initial comit

---
+++ routes/web.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+$html = <<<EOF
+<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<style>
+body {font-size:20pt; color:#999; }
+h1{ font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px;}
+
+</style>
+
+
+</head>
+<body>
+    <h1>Hello</h1>
+    <p>this is sample pages</p>
+</body>
+</html>
+EOF;
+
+Route::get('hello','HelloController@post');
+Route::post('hello','HelloController@post');
+
+// Route::get('hello', 'HelloController@index');
+
+// Route::get('hello/{id?}', 'helloController@index');
+
+
+// Route::get('hello', function(){
+//     return view('hello.index');
+// });
+
+
+
+// Route::get('hello', 'HelloController@index');
+
+
+// Route::get('hello', function () use ($html){
+//     return $html;
+// });
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
